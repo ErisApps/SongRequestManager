bdae40f0ccee1da2c8939e9d7531b6b189d263a8
Eris <erisapps@outlook.be>
2020-08-31 02:20:38 +0200

    Yeet old Mod Settings entry

--- SongRequestManager/Plugin.cs
+++ SongRequestManager/Plugin.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using BeatSaberMarkupLanguage.Settings;
 using IPA;
 using IPA.Config.Stores;
 using IPA.Loader;
@@ -7,7 +6,6 @@ using SemVer;
 using SongRequestManager.Extensions;
 using SongRequestManager.Settings;
 using SongRequestManager.Settings.Base;
-using SongRequestManager.Settings.UI;
 using Config = IPA.Config.Config;
 using Logger = SongRequestManager.Utilities.Logger;
 
@@ -19,8 +17,6 @@ namespace SongRequestManager
 		private static PluginMetadata? _metadata;
 		private static string? _name;
 		private static Version? _version;
-
-		private SettingsController? _settingsController;
 
 		public static string Name => _name ??= _metadata?.Name ?? Assembly.GetExecutingAssembly().GetName().Name;
 		public static Version Version => _version ??= _metadata?.Version ?? Assembly.GetExecutingAssembly().GetName().Version.ToSemVerVersion();
@@ -40,16 +36,11 @@ namespace SongRequestManager
 		{
 			SiraUtil.Zenject.Installer.RegisterAppInstaller<Installers.AppInstaller>();
 			SiraUtil.Zenject.Installer.RegisterMenuInstaller<Installers.MenuInstaller>();
-
-			BSMLSettings.instance.AddSettingsMenu("SRM (Alpha)", "SongRequestManager.Settings.UI.Settings.bsml", _settingsController ??= new SettingsController());
 		}
 
 		[OnDisable]
 		public void OnDisable()
 		{
-			BSMLSettings.instance.RemoveSettingsMenu(_settingsController);
-			_settingsController = null;
-
 			SiraUtil.Zenject.Installer.UnregisterMenuInstaller<Installers.MenuInstaller>();
 			SiraUtil.Zenject.Installer.UnregisterAppInstaller<Installers.AppInstaller>();
 		}
