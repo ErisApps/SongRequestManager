e43cb4335596d8c73748c24b81c738535de02cd1
Eris <erisapps@outlook.be>
2020-08-31 02:12:38 +0200

    Added ConfigChanged eventhandler to ReloadableConfig

--- SongRequestManager/Settings/Base/ReloadableBaseConfig.cs
+++ SongRequestManager/Settings/Base/ReloadableBaseConfig.cs
@@ -1,13 +1,20 @@
+using System;
+using SongRequestManager.Utilities;
+
 namespace SongRequestManager.Settings.Base
 {
 	internal abstract class ReloadableBaseConfig<T> : BaseConfig<T>
 	{
+		public event EventHandler ConfigChanged;
+
 		public virtual void OnReload()
 		{
 			// This is called whenever the config file is reloaded from disk.
 			// Use it to tell all of your systems that something has changed.
 
 			// This is called off of the main thread, and is not safe to interact with Unity in
+			Logger.Log($"{GetType().Name} got changed externally, reloading.");
+			ConfigChanged?.Invoke(this, EventArgs.Empty);
 		}
 	}
 }
