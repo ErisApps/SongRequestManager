52319e9d75fd4a0f7019460cbe97556fc857fa29
Eris <erisapps@outlook.be>
2020-09-10 21:38:59 +0200

    Added default values for the Twitch request limits

--- SongRequestManager/Settings/Partial/TwitchSettings.cs
+++ SongRequestManager/Settings/Partial/TwitchSettings.cs
@@ -3,9 +3,9 @@
 	internal class TwitchSettings
 	{
 		public virtual bool Enabled { get; set; } = true;
-		public virtual int UserRequestLimit { get; set; }
-		public virtual int SubRequestLimit { get; set; }
-		public virtual int ModRequestLimit { get; set; }
-		public virtual int VipBonusLimit { get; set; }
+		public virtual int UserRequestLimit { get; set; } = 10;
+		public virtual int SubRequestLimit { get; set; } = 20;
+		public virtual int ModRequestLimit { get; set; } = 25;
+		public virtual int VipBonusLimit { get; set; } = 3;
 	}
 }
